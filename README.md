9a6fd3155bb049947bdc0a8c2df34e4c21adf582
James Bubeck <bubeck@gmail.com>
2019-04-20 13:37:31 -0400

    add the script, example, and readme

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # expand_aliases
-Expand a mail /etc/aliases file so there's no nesting
+Expand a mail `/etc/aliases` file so there's no nesting
+
+Sometimes you want to know which aliases a given email address is a member 
+of.  You can do this by hand, but it is tedious if you have nested aliases. 
+This script takes a regular `/etc/aliases` file and resolves all aliases 
+down to their leaves so you can `grep` for an address to see what they're a
+member of.
+
+This doesn't explicitly support all alias features, such as pipes.  If you 
+have any leaves that aren't email addresses or paths, it will call it 
+"WEIRD" but will probably work anyway.
+
+## Usage
+
+```
+$ ./expand_aliases.py -h
+usage: expand_aliases.py [-h] [-c] aliases
+
+positional arguments:
+  aliases      aliases file
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -c, --count  show counts
+```
+
+### Example
+
+To expand an aliases file, just specify the input as an argument.
+```
+$ ./expand_aliases.py aliases_example
+nerds: sally@example.com,alice@example.com,bill@contoso.com,todd@example.com,susan@example.com,bob@example.com
+devs: sally@example.com,alice@example.com,bill@contoso.com
+admins: todd@example.com,susan@example.com
+eggheads: otis@example.com,sandra@example.com,kent@example.com,pat@example.com,bob@example.com
+hr: otis@example.com,sandra@example.com
+sales: kent@example.com,pat@example.com
+staff: sally@example.com,alice@example.com,bill@contoso.com,todd@example.com,susan@example.com,bob@example.com,otis@example.com,sandra@example.com,kent@example.com,pat@example.com,bob@example.com
+```
+
+If you want to know how many [local] members each alias has, specify 
+the `-c` flag:
+```
+$ ./expand_aliases.py -c aliases_example
+6 nerds: sally@example.com,alice@example.com,bill@contoso.com,todd@example.com,susan@example.com,bob@example.com
+3 devs: sally@example.com,alice@example.com,bill@contoso.com
+2 admins: todd@example.com,susan@example.com
+5 eggheads: otis@example.com,sandra@example.com,kent@example.com,pat@example.com,bob@example.com
+2 hr: otis@example.com,sandra@example.com
+2 sales: kent@example.com,pat@example.com
+11 staff: sally@example.com,alice@example.com,bill@contoso.com,todd@example.com,susan@example.com,bob@example.com,otis@example.com,sandra@example.com,kent@example.com,pat@example.com,bob@example.com
+```
---
+++ expand_aliases.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+"""
+expand_aliases.py
+
+Recursively expand out all aliases in specified file.  Only expands local 
+definitions - it does no remote magic with SMTP VRFY because that would be 
+bonkers.
+
+James B 2018-02-12
+"""
+
+import argparse
+import re
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("aliases", help="aliases file")
+parser.add_argument("-c", "--count", help="show counts", action="store_true")
+args = parser.parse_args()
+
+# Read aliases flat file into dict
+aliases = {}
+with open(args.aliases, 'r') as aliases_file:
+    for line in aliases_file:
+        # If not comment or blank line
+        if not (re.search('^\s*#', line) or re.search('^\s*$', line)):
+            search = re.search('^([^:]+):\s+(.*)', line.rstrip())
+            if not search:
+                sys.stderr.write("SEARCH DID NOT MATCH (line=%s)\n" % line)
+            else:
+                # left is alias name (left hand side of :)
+                left = search.group(1).lower()
+                # right is one or more comma separated alias
+                # members (right hand side of :)
+                right = search.group(2)
+                if left in aliases:
+                    sys.stderr.write("DUPLICATE ALIAS (line=%s)\n" % line)
+                else:
+                    members = right.split(",")
+                    aliases[left] = [m.strip() for m in members]
+
+# Recursively resolve alias into its members.  Does case-insensitive search.
+def resolve_alias(alias_name, aliases):
+    members = aliases[alias_name]
+    leaves = []
+    for member in members:
+        m = member.lower()
+        if m in aliases:
+            leaves.extend(resolve_alias(m, aliases))
+        else:
+            leaves.extend([member])
+    return leaves
+
+# Use resolve_aliases() to stitch together a complete aliases list down to leaves
+resolved_aliases = {}
+for alias_name in aliases.keys():
+    members = resolve_alias(alias_name, aliases)
+    resolved_aliases[alias_name] = members
+
+for alias, members in resolved_aliases.items():
+    if args.count:
+        num_members = len(members)
+        print("%d %s: %s" % (num_members, alias, ','.join(members)))
+    else:
+        print("%s: %s" % (alias, ','.join(members)))
+    for m in members:
+        if not (re.search('^[^@]+@[a-zA-Z0-9.-]+$', m) or re.search('^/', m)):
+            sys.stderr.write("WEIRD - no @ in alias '%s' member '%s'\n" % (alias, m))
+
